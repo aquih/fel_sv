529d7fb22a15215ac651b1812d6de992265c6eb9
Rodrigo Fernández <rodrigo@aquih.com>
2025-07-18 18:38:28 +0000

    Version 17.0

--- __manifest__.py
+++ __manifest__.py
@@ -2,17 +2,17 @@
 
 {
     'name': 'FEL El Salvador',
-    'version': '1.0',
+    'version': '2.0',
     'category': 'Custom',
     'description': """ Campos y funciones base para la facturación electrónica en El Salvador """,
     'author': 'aquíH',
     'website': 'http://aquih.com/',
     'depends': ['l10n_sv'],
     'data': [
-        'views/account_view.xml',
-        'views/res_company_view.xml',
-        'views/partner_view.xml',
-        'views/res_country_view.xml',
+        'views/account_views.xml',
+        'views/res_company_views.xml',
+        'views/partner_views.xml',
+        'views/res_country_views.xml',
     ],
     'demo': [],
     'installable': True
